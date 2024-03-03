0e95986daac7296b03f1720396d5d0804ed13a9c
HimanshuSarma <sweetiehimanshu@gmail.com>
2024-03-03 20:46:10 +0530

    3rd-commit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,9 +5,6 @@ import { groupRouter } from './group.js';
 
 const router: Router = Router();
 
-router.use(`/`, (req, res) => {
-    return res?.status(200).send('Hi');
-});
 router.use(`/user`, userRouter);
 router.use(`/group`, groupRouter);
 
