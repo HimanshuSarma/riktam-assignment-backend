e61ecfbcf23340f5d1c7aad14ac72a10f843ba6f
HimanshuSarma <sweetiehimanshu@gmail.com>
2024-03-03 20:38:09 +0530

    2nd-commit

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -2,6 +2,9 @@ import { Router } from 'express';
 import { userRouter } from './user.js';
 import { groupRouter } from './group.js';
 const router = Router();
+router.use(`/`, (req, res) => {
+    return res === null || res === void 0 ? void 0 : res.status(200).send('Hi');
+});
 router.use(`/user`, userRouter);
 router.use(`/group`, groupRouter);
 export { router };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,9 @@ import { groupRouter } from './group.js';
 
 const router: Router = Router();
 
+router.use(`/`, (req, res) => {
+    return res?.status(200).send('Hi');
+});
 router.use(`/user`, userRouter);
 router.use(`/group`, groupRouter);
 
